be659894d38eb7b04b700b7c4641d1fe262f1551
Lucstarwork <lucas.p6259@gmail.com>
2026-06-30 18:06:05 -0300

    feat: ajustes versão 1.1.8

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path');
 
@@ -37,11 +37,31 @@ function configurarAutoUpdate() {
   autoUpdater.autoDownload = true;
   autoUpdater.autoInstallOnAppQuit = false;
 
-  autoUpdater.on('update-downloaded', () => {
-    autoUpdater.quitAndInstall(true, true);
+  autoUpdater.on('checking-for-update', () => console.log('[updater] verificando...'));
+  autoUpdater.on('update-available', (i) => console.log('[updater] disponível:', i.version));
+  autoUpdater.on('update-not-available', (i) => console.log('[updater] sem update, versão atual:', i.version));
+  autoUpdater.on('error', (err) => console.log('[updater] erro:', err.message));
+  autoUpdater.on('download-progress', (p) => console.log('[updater] download:', Math.round(p.percent) + '%'));
+  autoUpdater.on('update-downloaded', async () => {
+    console.log('[updater] download concluído, aguardando confirmação do usuário...');
+    const { response } = await dialog.showMessageBox(mainWindow, {
+      type: 'info',
+      buttons: ['Instalar agora', 'Mais tarde'],
+      defaultId: 0,
+      cancelId: 1,
+      title: 'Atualização pronta',
+      message: 'Uma nova versão foi baixada e está pronta para instalar.',
+      detail: 'Clique em "Instalar agora" para reiniciar o aplicativo e aplicar a atualização.'
+    });
+    if (response === 0) {
+      console.log('[updater] usuário confirmou — instalando...');
+      autoUpdater.quitAndInstall(true, true);
+    } else {
+      console.log('[updater] usuário adiou a instalação.');
+    }
   });
 
-  autoUpdater.checkForUpdates().catch(() => {});
+  autoUpdater.checkForUpdates().catch((err) => console.log('[updater] checkForUpdates falhou:', err.message));
 }
 
 app.whenReady().then(() => {
